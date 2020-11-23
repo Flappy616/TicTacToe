fce67a08af8181816e6a7a094d50d6d666671cad
flappy <fabrizio.florio89@gmail.com>
2020-11-23 15:24:46 +0100

    add tic tac toe

---
+++ Modules/ClassBoard.py
@@ -0,0 +1,56 @@
+class Board:
+    
+    def __init__(self):
+        self.state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    def make_turn(self, cell, player):
+        if self.is_valid_turn(cell):
+            self.state[cell] = player.symbol
+            return True
+        return False    
+
+    def is_valid_turn(self, cell):
+        if self.state[cell] == 0:
+            return True
+        else:
+            return False
+
+    def check_win(self, player):
+        s = player.symbol
+        if self.state[0] == s and self.state[1] == s and self.state[2] == s:
+            return True
+        elif self.state[3] == s and self.state[4] == s and self.state[5] == s:
+            return True
+        elif self.state[6] == s and self.state[7] == s and self.state[8] == s:
+            return True
+
+        elif self.state[0] == s and self.state[3] == s and self.state[6] == s:
+            return True
+        elif self.state[1] == s and self.state[4] == s and self.state[7] == s:
+            return True
+        elif self.state[2] == s and self.state[5] == s and self.state[8] == s:
+            return True
+
+        elif self.state[0] == s and self.state[4] == s and self.state[8] == s:
+            return True
+        elif self.state[6] == s and self.state[4] == s and self.state[2] == s:
+            return True
+
+    def is_full(self):
+        for i in self.state:
+            if i == 0:
+                return False
+        return True
+
+    def sign_to_printable(self, sign):
+        if sign ==0:
+            return " "
+        elif sign == 1:
+            return "X"
+        else:
+            return "O"
+
+    def print_board(self):
+        print(  " "+ self.sign_to_printable(self.state[0]) +" | "+ self.sign_to_printable(self.state[1]) +" | "+ self.sign_to_printable(self.state[2]) +" \n" +
+                " "+ self.sign_to_printable(self.state[3]) +" | "+ self.sign_to_printable(self.state[4]) +" | "+ self.sign_to_printable(self.state[5]) +" \n" +
+                " "+ self.sign_to_printable(self.state[6]) +" | "+ self.sign_to_printable(self.state[7]) +" | "+ self.sign_to_printable(self.state[8]) +" \n" )
---
+++ tictactoe.py
@@ -0,0 +1,32 @@
+from Modules.ClassBoard import Board
+from Modules.ClassPlayer import Player
+
+if __name__ == '__main__':
+    player_1 = Player(1)
+    player_2 = Player(-1)
+    board = Board()
+    active_player = player_1
+
+    while not board.is_full():
+        board.print_board()
+        try:
+            cell = int(input("Wo willst du den Zeichen setzen? (Zahl 1 - 9)"))
+        except ValueError:
+            continue
+        cell = cell - 1
+        if cell < 0 or cell > 8:
+            print("Bitte gib eine Zahl zwischen 1 und 9 an!") 
+            continue
+        if not board.make_turn(cell, active_player):
+            print("das Feld ist bereits Vergeben!")
+            continue
+
+        if board.check_win(active_player):
+            print("Du hast Gewonnen! GG!")
+            print(board.print_board())
+            exit()
+
+        if active_player == player_1:
+            active_player = player_2
+        else:
+            active_player = player_1
